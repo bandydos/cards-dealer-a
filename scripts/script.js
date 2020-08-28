c123d5f6dd4c3d49bb2be1411c2b63ba1076eb52
bandydos <alexandertack@hotmail.be>
2020-08-28 14:59:29 +0200

    Part of visualisation done

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,32 @@
 $(document).ready(() => {
+    $('#btn-shuffle').click(() => {
+        const inputTables = parseInt($('#input-tables').val());
+        const inputPlayers = parseInt($('#input-players').val());
+        if (inputTables && inputPlayers) {
+            const tables = generateTables(inputTables, inputPlayers);
+            shuffle(tables);
+            for (let i = 0; i < tables.length; i++) {
+                const column = '<div class="col-3 mt-5">';
+                const thead = `<table><thead><th>${tables[i].name}<tr><th>#<th>cards`;
+                $('#div-tables').append(column + thead)
 
+                const players = tables[i].players;
+                for (let j = 0; j < players.length; j++) {
+                    const playerName = players[j].name;
+                    const playerHand = players[j].hand;
+                    const tbodyPlayerName = `<tbody><tr><td>${playerName}`;
+                    $('#div-tables').append(tbodyPlayerName)
+                    for (let k = 0; k < playerHand.length; k++) {
+                        const card = `${playerHand[k].name} of ${playerHand[k].suit}`;
+                        const tbodyPlayerCards = `<tbody><td>${card}`;
+                        $('#div-tables').append(tbodyPlayerCards)
+                    }
+                }
+            }
+        } else {
+            console.log('error')
+        }
+    })
 })
 
 const generateDeck = (amountOfDecks) => {
@@ -37,7 +64,7 @@ const generateTables = (amountOfTables, amountOfPlayersPerTable) => {
         }
         for (let j = 0; j < amountOfPlayersPerTable; j++) {
             table.players.push({
-                name: 'P' + j,
+                name: 'P' + (j + 1),
                 hand: [] // Hand for every player.
             })
         }
@@ -57,13 +84,8 @@ const shuffle = (tbls) => {
     for (let i = 0; i < 2; i++) {
         for (let j = 0; j < tbls.length; j++) {
             for (let k = 0; k < tbls[j].players.length; k++) {
-                pickCard(tbls[j].cards, tbls[j].players[k].hand); // [k][0] because of nesting.
+                pickCard(tbls[j].cards, tbls[j].players[k].hand);
             }
         }
     }
 }
-
-const tbls = generateTables(5, 6);
-shuffle(tbls);
-console.log(tbls)
-
